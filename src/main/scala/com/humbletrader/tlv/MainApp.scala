ac657fcafa9fbd5dce31d6c71d3d59e6f565739b
dragosbadswizz <dragos.balan@adswizz.com>
2023-09-15 15:55:55 +0300

    project boilerplate

---
+++ src/main/scala/com/humbletrader/tlv/MainApp.scala
@@ -0,0 +1,9 @@
+package com.humbletrader.tlv
+
+object MainApp {
+
+  def main(args: Array[String]): Unit = {
+    println("application started...")
+  }
+
+}
---
+++ src/test/scala/com/humbletrader/tlv/MainAppTest.scala
@@ -0,0 +1,12 @@
+package com.humbletrader.tlv
+
+import org.scalatest.FunSuite
+
+class MainAppTest extends FunSuite{
+
+    test("blah"){
+      println("running tests...")
+    }
+
+
+}
